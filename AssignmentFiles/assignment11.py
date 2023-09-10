2fd6f2992a0eb1da61eb22884d5a5e5eb717752d
avianage <89604905+avianage@users.noreply.github.com>
2023-09-10 22:36:49 +0530

    Added Assignment 11 Code and Output

---
+++ AssignmentFiles/assignment11.py
@@ -0,0 +1,13 @@
+
+# To generate pseudo random numbers.
+
+import random
+
+random_integer = random.randint(1, 100)
+print("\nRandom Integer between 1 and 100:", random_integer)
+
+random_float = random.random()
+print("Random Float:", random_float)
+
+random_range = random.uniform(2.0, 5.0)
+print("Random Number in Range 2.0 and 5.0:", random_range)
